e220f4f47cf747e62ad53781942eeba743ed8532
tracer-ics2022 <tracer@njuics.org>
2024-07-07 13:41:50 +0800

    >  compile NEMU 23030112 puji Linux pu 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:41:50 up 12:40,  1 user,  load average: 1.03, 1.08, 1.08

--- abstract-machine/am/src/riscv/spike/htif.c
+++ abstract-machine/am/src/riscv/spike/htif.c
@@ -87,7 +87,7 @@ void htif_syscall(uintptr_t arg)
 
 void htif_console_putchar(uint8_t ch)
 {
-//#if __riscv_xlen == 32
+#if __riscv_xlen == 32
   // HTIF devices are not supported on RV32, so proxy a write system call
   volatile uint64_t magic_mem[8];
   magic_mem[0] = SYS_write;
@@ -95,11 +95,11 @@ void htif_console_putchar(uint8_t ch)
   magic_mem[2] = (uintptr_t)&ch;
   magic_mem[3] = 1;
   do_tohost_fromhost(0, 0, (uintptr_t)magic_mem);
-// #else
-//   spinlock_lock(&htif_lock);
-//     __set_tohost(1, 1, ch);
-//   spinlock_unlock(&htif_lock);
-//#endif
+#else
+  spinlock_lock(&htif_lock);
+    __set_tohost(1, 1, ch);
+  spinlock_unlock(&htif_lock);
+#endif
 }
 
 void htif_poweroff()
