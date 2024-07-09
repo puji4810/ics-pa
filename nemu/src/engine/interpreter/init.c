2cfd7029d882096a08da4f1c859deeecd058649f
tracer-ics2022 <tracer@njuics.org>
2024-07-09 22:11:21 +0800

    >  compile NEMU 23030112 puji Linux pu 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:11:21 up  3:32,  1 user,  load average: 1.12, 1.18, 1.11

--- nemu/src/engine/interpreter/init.c
+++ nemu/src/engine/interpreter/init.c
@@ -16,12 +16,14 @@
 #include <cpu/cpu.h>
 
 void sdb_mainloop();
+void sdb_set_batch_mode();
 
 void engine_start() {
 #ifdef CONFIG_TARGET_AM
   cpu_exec(-1);
 #else
   /* Receive commands from user. */
+  sdb_set_batch_mode();
   sdb_mainloop();
 #endif
 }
