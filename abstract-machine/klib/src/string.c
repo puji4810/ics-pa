8e0549b21e4d1282355b5301acc0b14afb6d9f1a
tracer-ics2022 <tracer@njuics.org>
2024-07-10 20:10:04 +0800

    >  compile NEMU 23030112 puji Linux pu 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:10:04 up  8:13,  1 user,  load average: 1.97, 1.44, 1.22

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -4,119 +4,163 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-size_t strlen(const char *s) {
-  if(s == NULL) 
+// 计算一个字符串的长度
+size_t strlen(const char *s)
+{
+  if (s == NULL)
+  {
     return 0;
-  else {
-    size_t len = 0;
-    while(s[len] != '\0') len++;
-    return len;
   }
+  size_t n = 0;
+  while (s[n] != '\0')
+  {
+    ++n;
+  }
+  return n;
 }
 
-char *strcpy(char *dst, const char *src) {
-  if(src == NULL) return NULL;
-  else {
-    char *ret = dst;
-    while((*dst++ = *src++) != '\0');
-    return ret;
+// 将一个字符串复制到另一个字符串中
+char *strcpy(char *dst, const char *src)
+{
+  if (src == NULL || dst == NULL)
+  { // 没有所指，直接返回dst
+    return dst;
   }
+  // 当成指向字符数组处理，所以即使没有空字符，导致内存访问越界，或修改了其他有用的数据也不管，因为这是函数调用者所需要保证的，下面一些string函数都是这样对带非字符串数组
+  char *res = dst;
+  do
+  {
+    *dst = *src;
+    dst++;
+    src++;
+  } while (*src != '\0');
+  return res;
 }
 
-char *strncpy(char *dst, const char *src, size_t n) {
-  if(src == NULL) return NULL;
-  else {
-    char *ret = dst;
-    while(*src != '\0') 
-    {
-      if(n-- == 0) break;
-      *dst++ = *src++;
-    }
-    *dst = '\0';
-    return ret;
+// 将源字符串的一部分复制到目标字符串中
+char *strncpy(char *dst, const char *src, size_t n)
+{
+  size_t i;
+  for (i = 0; src[i] != '\0'; i++)
+  {
+    dst[i] = src[i];
   }
+
+  dst[i] = '\0';
+  return dst;
 }
 
-char *strcat(char *dst, const char *src) {
-  if(src == NULL) return dst;
-  else {
-    char *ret = dst;
-    while(*dst != '\0') dst++;
-    while((*dst++ = *src++) != '\0');
-    return ret;
+// 用于将一个字符串追加到另一个字符串的末尾
+char *strcat(char *dst, const char *src)
+{
+  size_t i = 0;
+  while (dst[i] != '\0')
+  {
+    i++;
   }
+
+  strcpy(dst + i, src);
+  return dst;
 }
 
-int strcmp(const char *s1, const char *s2) {
-  if(s1 == NULL && s2 == NULL) return 0;
-  else if(s1 == NULL) return -1;
-  else if(s2 == NULL) return 1;
-  else {
-    while(*s1 != '\0' && *s2 != '\0') {
-      if(*s1 < *s2) return -1;
-      else if(*s1 > *s2) return 1;
-      s1++;
-      s2++;
-    }
-    if(*s1 == '\0' && *s2 == '\0') return 0;
-    else if(*s1 == '\0') return -1;
-    else return 1;
+// 用于比较两个字符串的内容
+int strcmp(const char *s1, const char *s2)
+{
+  size_t i = 0;
+  while (s1[i] != '\0' && s2[i] != '\0')
+  {
+    if (s1[i] > s2[i])
+      return 1;
+    if (s1[i] < s2[i])
+      return -1;
+    i++;
   }
+  if (s1[i] != '\0' && s2[i] == '\0')
+    return 1;
+  if (s1[i] == '\0' && s2[i] != '\0')
+    return -1;
+  return 0;
 }
 
-int strncmp(const char *s1, const char *s2, size_t n) {
-  if(s1 == NULL && s2 == NULL) return 0;
-  else if(s1 == NULL) return -1;
-  else if(s2 == NULL) return 1;
-  else {
-    while(*s1 != '\0' && *s2 != '\0' && n-- > 0) {
-      if(*s1 < *s2) return -1;
-      else if(*s1 > *s2) return 1;
-      s1++;
-      s2++;
-    }
-    if(n == 0) return 0;
-    if(*s1 == '\0' && *s2 == '\0') return 0;
-    else if(*s1 == '\0') return -1;
-    else return 1;
+// 比较两个字符串的前几个字符的内容
+int strncmp(const char *s1, const char *s2, size_t n)
+{
+  while (n--)
+  {
+    if (*s1 > *s2)
+      return 1;
+    if (*s1 < *s2)
+      return -1;
+    s1++;
+    s2++;
   }
+  return 0;
 }
 
-void *memset(void *s, int c, size_t n) {
-  char *p = (char *)s;
-  while(n-- > 0) *p++ = c;
+// 将指定的内存区域的每个字节都设置为特定的值
+void *memset(void *s, int c, size_t n)
+{
+  char *ch = (char *)s;
+  while (n-- > 0)
+    *ch++ = c;
   return s;
 }
 
-void *memmove(void *dst, const void *src, size_t n) {
-  char *d = (char *)dst;
-  const char *s = (const char *)src;
-  if(d < s) {
-    while(n-- > 0) *d++ = *s++;
+// 用于在内存中移动一块数据。与 memcpy 函数不同，memmove 能够处理源内存区域与目标内存区域重叠的情况，以确保数据正确移动
+void *memmove(void *dst, const void *src, size_t n)
+{
+  if (dst < src)
+  {
+    char *d = (char *)dst;
+    char *s = (char *)src;
+    while (n--)
+    {
+      *d = *s;
+      d++;
+      s++;
+    }
   }
-  else {
-    d += n;
-    s += n;
-    while(n-- > 0) *--d = *--s;
+  else
+  {
+    char *d = (char *)dst + n - 1;
+    char *s = (char *)src + n - 1;
+    while (n--)
+    {
+      *d = *s;
+      d--;
+      s--;
+    }
   }
   return dst;
 }
 
-void *memcpy(void *out, const void *in, size_t n) {
+// 将一个内存区域的内容复制到另一个内存区域
+void *memcpy(void *out, const void *in, size_t n)
+{
   char *d = (char *)out;
-  const char *s = (const char *)in;
-  while(n-- > 0) *d++ = *s++;
+  char *s = (char *)in;
+  while (n--)
+  {
+    *d = *s;
+    d++;
+    s++;
+  }
   return out;
 }
 
-int memcmp(const void *s1, const void *s2, size_t n) {
-  const unsigned char *p1 = (const unsigned char *)s1;
-  const unsigned char *p2 = (const unsigned char *)s2;
-  while(n-- > 0) {
-    if(*p1 < *p2) return -1;
-    else if(*p1 > *p2) return 1;
-    p1++;
-    p2++;
+// 用于比较两个内存区域的内容
+int memcmp(const void *s1, const void *s2, size_t n)
+{
+  char *S1 = (char *)s1;
+  char *S2 = (char *)s2;
+  while (n--)
+  {
+    if (*S1 > *S2)
+      return 1;
+    if (*S1 < *S2)
+      return -1;
+    S1++;
+    S2++;
   }
   return 0;
 }
