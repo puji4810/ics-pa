8230d74af0c6d10cd9ecde112ef61c35c0123c4d
tracer-ics2022 <tracer@njuics.org>
2024-07-10 21:26:41 +0800

    >  compile NEMU 23030112 puji Linux pu 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:26:41 up  9:30,  1 user,  load average: 1.21, 1.26, 1.15

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -9,12 +9,97 @@ int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+int vsprintf(char *out, const char *fmt, va_list ap)
+{
+  char *p = out;
+  const char *f = fmt;
+  while (*f)
+  {
+    if (*f != '%')
+    {
+      *p++ = *f++;
+      continue;
+    }
+    f++;
+    switch (*f)
+    {
+    case 's':
+    {
+      const char *s = va_arg(ap, const char *);
+      while (*s)
+      {
+        *p++ = *s++;
+      }
+      break;
+    }
+    case 'd':
+    {
+      int d = va_arg(ap, int);
+      if (d < 0)
+      {
+        *p++ = '-';
+        d = -d;
+      }
+      char buf[32];
+      int i = 0;
+      if (d == 0)
+      {
+        buf[i++] = '0';
+      }
+      while (d)
+      {
+        buf[i++] = d % 10 + '0';
+        d /= 10;
+      }
+      while (i--)
+      {
+        *p++ = buf[i];
+      }
+      break;
+    }
+    case 'x':
+    {
+      unsigned int x = va_arg(ap, unsigned int);
+      char buf[32];
+      int i = 0;
+      if (x == 0)
+      {
+        buf[i++] = '0';
+      }
+      while (x)
+      {
+        int t = x % 16;
+        if (t < 10)
+        {
+          buf[i++] = t + '0';
+        }
+        else
+        {
+          buf[i++] = t - 10 + 'a';
+        }
+        x /= 16;
+      }
+      while (i--)
+      {
+        *p++ = buf[i];
+      }
+      break;
+    }
+    default:
+      panic("Not implemented");
+    }
+    f++;
+  }
+  *p = '\0';
+  return p - out;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  int ret = vsprintf(out, fmt, ap);
+  va_end(ap);
+  return ret;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
