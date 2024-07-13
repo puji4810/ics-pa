3c4a6bd9f3902f0e7c915fd949fb02af4c3629e9
tracer-ics2022 <tracer@njuics.org>
2024-07-14 00:07:55 +0800

    >  compile NEMU 23030112 puji Linux pu 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:07:55 up 13 min,  1 user,  load average: 1.15, 1.15, 0.75

--- nemu/src/utils/trace.c
+++ nemu/src/utils/trace.c
@@ -44,3 +44,13 @@ void display_inst()
 	} while ((i = (i + 1) % MAX_IRINGBUF) != end);
 	puts(ANSI_NONE);
 }
+
+void display_pread(paddr_t addr, int len)
+{
+	printf("pread at " FMT_PADDR " len=%d\n", addr, len);
+}
+
+void display_pwrite(paddr_t addr, int len, word_t data)
+{
+	printf("pwrite at " FMT_PADDR " len=%d, data=" FMT_WORD "\n", addr, len, data);
+}
