937c7d6a865b533cc76f7976d0b71313141e451e
tracer-ics2022 <tracer@njuics.org>
2024-07-14 00:22:44 +0800

    >  compile NEMU 23030112 puji Linux pu 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:22:44 up 28 min,  1 user,  load average: 0.96, 1.23, 1.10

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -58,7 +58,7 @@ void init_mem() {
 
 word_t paddr_read(paddr_t addr, int len) {
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
-  IFDEF(CONFIG_MTRACE, mtrace_pread(addr, len));
+  //IFDEF(CONFIG_MTRACE, mtrace_pread(addr, len));
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
   return 0;
@@ -66,7 +66,7 @@ word_t paddr_read(paddr_t addr, int len) {
 
 void paddr_write(paddr_t addr, int len, word_t data) {
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
-  IFDEF(CONFIG_MTRACE, mtrace_pwrite(addr, len));
+  //IFDEF(CONFIG_MTRACE, mtrace_pwrite(addr, len));
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
 }
