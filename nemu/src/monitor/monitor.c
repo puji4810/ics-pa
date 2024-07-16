7d270edff3cbd64ebe5e6b6ca4360d7b84aac205
tracer-ics2022 <tracer@njuics.org>
2024-07-17 01:39:31 +0800

    >  compile NEMU 23030112 puji Linux pu 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:39:31 up 13:33,  1 user,  load average: 1.16, 1.17, 1.09

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -89,12 +89,8 @@ static int parse_args(int argc, char *argv[]) {
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
-      case 'e': printf("\n%s\n",optarg);elf_file = optarg;
-        printf("\n%s\n", elf_file);
-        break;
-      case 1: img_file = optarg;
-        printf("\n%s\n", log_file);
-        return 0;
+      case 'e': elf_file = optarg; break;
+      case 1: img_file = optarg; return 0;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
         printf("\t-b,--batch              run with batch mode\n");
@@ -111,7 +107,7 @@ static int parse_args(int argc, char *argv[]) {
 
 void init_monitor(int argc, char *argv[]) {
   /* Perform some global initialization. */
-  printf("111111111111%s %s\n", elf_file,log_file);
+
   /* Parse ELF file. */
   parse_elf(elf_file);
 
