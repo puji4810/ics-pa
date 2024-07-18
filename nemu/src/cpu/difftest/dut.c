3e58a6d88406e5ccaea654c7dcbbbfe27c40854c
tracer-ics2022 <tracer@njuics.org>
2024-07-18 20:34:12 +0800

    >  compile NEMU 23030112 puji Linux pu 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:34:12 up 1 day,  8:43,  1 user,  load average: 1.10, 1.08, 1.11

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -31,6 +31,26 @@ void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
 static bool is_skip_ref = false;
 static int skip_dut_nr_inst = 0;
 
+bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc)
+{
+  bool sign = true;
+  int i = 0;
+  for (; i < 32; i++)
+  {
+    if (cpu.gpr[i] != ref_r->gpr[i])
+    {
+      sign = false;
+      break;
+    }
+  }
+  if (sign && cpu.pc == ref_r->pc)
+  {
+    return true;
+  }
+  pc = ref_r->pc;
+  return false;
+}
+
 // this is used to let ref skip instructions which
 // can not produce consistent behavior with NEMU
 void difftest_skip_ref() {
@@ -131,22 +151,3 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
 void init_difftest(char *ref_so_file, long img_size, int port) { }
 #endif
 
-bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc)
-{
-  bool sign = true;
-  int i = 0;
-  for (; i < 32; i++)
-  {
-    if (cpu.gpr[i] != ref_r->gpr[i])
-    {
-      sign = false;
-      break;
-    }
-  }
-  if (sign && cpu.pc == ref_r->pc)
-  {
-    return true;
-  }
-  pc = ref_r->pc;
-  return false;
-}
