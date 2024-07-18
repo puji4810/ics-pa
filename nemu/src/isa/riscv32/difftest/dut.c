6dc1a57384c353d1a9035fba2881a673f3ff638d
tracer-ics2022 <tracer@njuics.org>
2024-07-18 20:38:27 +0800

    >  compile NEMU 23030112 puji Linux pu 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:38:27 up 1 day,  8:47,  1 user,  load average: 1.25, 1.20, 1.15

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -17,9 +17,26 @@
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
 
-bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
-}
 
 void isa_difftest_attach() {
+}
+
+bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc)
+{
+  bool sign = true;
+  int i = 0;
+  for (; i < 32; i++)
+  {
+    if (cpu.gpr[i] != ref_r->gpr[i])
+    {
+      sign = false;
+      break;
+    }
+  }
+  if (sign && cpu.pc == ref_r->pc)
+  {
+    return true;
+  }
+  pc = ref_r->pc;
+  return false;
 }
