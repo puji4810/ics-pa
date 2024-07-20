d56e4bba0ef6231d0bf264dbd6e1b33f5c7e3272
tracer-ics2022 <tracer@njuics.org>
2024-07-20 20:42:17 +0800

    >  compile NEMU 23030112 puji Linux pu 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:42:17 up  9:06,  1 user,  load average: 1.08, 1.08, 1.07

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -69,7 +69,7 @@ word_t map_read(paddr_t addr, int len, IOMap *map)
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
   word_t ret = host_read(map->space + offset, len);
-  IFDEF(CONFIG_DIFFTEST, dtrace_read(addr,map));
+  IFDEF(CONFIG_DTRACE, dtrace_read(addr,map));
   return ret;
 }
 
@@ -79,5 +79,5 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
   invoke_callback(map->callback, offset, len, true);
-  IFDEF(CONFIG_DIFFTEST, dtrace_write(addr, map));
+  IFDEF(CONFIG_DTRACE, dtrace_write(addr, map));
 }
