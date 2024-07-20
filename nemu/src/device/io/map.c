e0dab85077ff02f57df10b2b66dab9cf5773760c
tracer-ics2022 <tracer@njuics.org>
2024-07-20 20:37:03 +0800

    >  compile NEMU 23030112 puji Linux pu 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:37:03 up  9:01,  1 user,  load average: 1.00, 1.06, 1.06

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -52,15 +52,7 @@ void init_map() {
   p_space = io_space;
 }
 
-void dtrace_read(paddr_t addr, IOMap *map)
-{
-  printf("dtrace: read at " FMT_PADDR " from %s\n", addr, map->name);
-}
 
-void dtrace_write(paddr_t addr, IOMap *map)
-{
-  printf("dtrace: write at " FMT_PADDR " to %s\n", addr, map->name);
-}
 
 word_t map_read(paddr_t addr, int len, IOMap *map)
 {
@@ -69,7 +61,7 @@ word_t map_read(paddr_t addr, int len, IOMap *map)
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
   word_t ret = host_read(map->space + offset, len);
-  dtrace_read(addr, map);
+  //dtrace_read(addr, map);
   return ret;
 }
 
@@ -79,5 +71,5 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
   invoke_callback(map->callback, offset, len, true);
-  dtrace_write(addr,map);
+  //dtrace_write(addr,map);
 }
